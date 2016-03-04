c4c06c2ea9d7d62f72383a1845f9b1fe37053d78
iOkay <daiyunxing@pwrd.com>
2016-03-04 14:20:13 +0800

    First commit

---
+++ RefactorDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RefactorDemo
+//
+//  Created by Bran on 16/3/4.
+//  Copyright © 2016年 Bran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RefactorDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RefactorDemo
+//
+//  Created by Bran on 16/3/4.
+//  Copyright © 2016年 Bran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
