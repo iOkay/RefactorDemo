2bb146eb9d0ce327325f548578f9fc8402daf30f
iOkay <daiyunxing@pwrd.com>
2016-03-09 16:44:42 +0800

    首先声明一个新类。新类中提供一个字段来保存源对象；对于函数中的每一个参数和每一个临时变量，也以一个字段一一保存。

---
+++ RefactorDemo/Gamma.h
@@ -0,0 +1,13 @@
+//
+//  Gamma.h
+//  RefactorDemo
+//
+//  Created by Bran on 16/3/9.
+//  Copyright © 2016年 Bran. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Gamma : NSObject
+
+@end
---
+++ RefactorDemo/Gamma.m
@@ -0,0 +1,28 @@
+//
+//  Gamma.m
+//  RefactorDemo
+//
+//  Created by Bran on 16/3/9.
+//  Copyright © 2016年 Bran. All rights reserved.
+//
+
+#import "Gamma.h"
+
+#import "Account.h"
+
+@interface Gamma ()
+{
+    __weak Account *account;
+    int inputVal;
+    int quantity;
+    int yearToDate;
+    int importantValue1;
+    int importantValue2;
+    int importantValue3;
+}
+
+@end
+
+@implementation Gamma
+
+@end
